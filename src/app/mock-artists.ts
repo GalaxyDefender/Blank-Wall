2e8456258008a69f5e63524ba220979acc50c375
GalaxyDefender <qgerard.gerard@gmail.com>
2017-11-08 14:45:09 +0200

    mock artist file added

---
+++ src/app/mock-artists.ts
@@ -0,0 +1,259 @@
+import { Artist } from './artist';
+
+export const ARTISTS: Artist[] = [
+      {
+        "id": "598ca9a52a893a58e5298b7d",
+        "slug": "raquel-alvarez-sardina",
+        "created_at": "2017-08-10T18:44:53+00:00",
+        "updated_at": "2017-10-31T14:23:43+00:00",
+        "name": "Raquel Alvarez Sardina",
+        "sortable_name": "Alvarez Sardina Raquel",
+        "gender": "female",
+        "biography": "",
+        "birthday": "1967",
+        "deathday": "",
+        "hometown": "Barcelona, Spain",
+        "location": "Stroud, United Kingdom",
+        "nationality": "Spanish",
+        "image_versions": [
+          "square",
+          "four_thirds",
+          "large",
+          "tall"
+        ],
+        "_links": {
+          "thumbnail": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/Sazh9DfJcJiFy2IVjMzfJQ/square.jpg"
+          },
+          "image": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/Sazh9DfJcJiFy2IVjMzfJQ/{image_version}.jpg",
+            "templated": true
+          },
+          "self": {
+            "href": "https://api.artsy.net/api/artists/598ca9a52a893a58e5298b7d"
+          },
+          "permalink": {
+            "href": "http://www.artsy.net/artist/raquel-alvarez-sardina"
+          },
+          "artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=598ca9a52a893a58e5298b7d"
+          },
+          "published_artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=598ca9a52a893a58e5298b7d&published=true"
+          },
+          "similar_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=598ca9a52a893a58e5298b7d"
+          },
+          "similar_contemporary_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=598ca9a52a893a58e5298b7d&similarity_type=contemporary"
+          },
+          "genes": {
+            "href": "https://api.artsy.net/api/genes?artist_id=598ca9a52a893a58e5298b7d"
+          }
+        }
+      },
+      {
+        "id": "58595c498b3b812bf50000d9",
+        "slug": "fides-becker",
+        "created_at": "2016-12-20T16:28:57+00:00",
+        "updated_at": "2017-10-31T14:36:58+00:00",
+        "name": "Fides Becker",
+        "sortable_name": "Becker Fides",
+        "gender": "female",
+        "biography": "",
+        "birthday": "1962",
+        "deathday": "",
+        "hometown": "",
+        "location": "Berlin, Germany",
+        "nationality": "German",
+        "image_versions": [
+          "square",
+          "tall",
+          "four_thirds",
+          "large"
+        ],
+        "_links": {
+          "thumbnail": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/wlrk4RDUJrz0MXtXE0lF8g/square.jpg"
+          },
+          "image": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/wlrk4RDUJrz0MXtXE0lF8g/{image_version}.jpg",
+            "templated": true
+          },
+          "self": {
+            "href": "https://api.artsy.net/api/artists/58595c498b3b812bf50000d9"
+          },
+          "permalink": {
+            "href": "http://www.artsy.net/artist/fides-becker"
+          },
+          "artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=58595c498b3b812bf50000d9"
+          },
+          "published_artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=58595c498b3b812bf50000d9&published=true"
+          },
+          "similar_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=58595c498b3b812bf50000d9"
+          },
+          "similar_contemporary_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=58595c498b3b812bf50000d9&similarity_type=contemporary"
+          },
+          "genes": {
+            "href": "https://api.artsy.net/api/genes?artist_id=58595c498b3b812bf50000d9"
+          }
+        }
+      },
+      {
+        "id": "5857f54a9c18db601c001594",
+        "slug": "alberto-morrocco",
+        "created_at": "2016-12-19T14:57:14+00:00",
+        "updated_at": "2017-10-31T14:22:15+00:00",
+        "name": "Alberto Morrocco",
+        "sortable_name": "Morrocco Alberto",
+        "gender": "male",
+        "biography": "",
+        "birthday": "1917",
+        "deathday": "1998",
+        "hometown": "Aberdeen, United Kingdom",
+        "location": "",
+        "nationality": "Scottish",
+        "image_versions": [
+          "square",
+          "four_thirds",
+          "tall",
+          "large"
+        ],
+        "_links": {
+          "thumbnail": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/QQWkDyH4LBChMhcYodQmlw/square.jpg"
+          },
+          "image": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/QQWkDyH4LBChMhcYodQmlw/{image_version}.jpg",
+            "templated": true
+          },
+          "self": {
+            "href": "https://api.artsy.net/api/artists/5857f54a9c18db601c001594"
+          },
+          "permalink": {
+            "href": "http://www.artsy.net/artist/alberto-morrocco"
+          },
+          "artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=5857f54a9c18db601c001594"
+          },
+          "published_artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=5857f54a9c18db601c001594&published=true"
+          },
+          "similar_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=5857f54a9c18db601c001594"
+          },
+          "similar_contemporary_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=5857f54a9c18db601c001594&similarity_type=contemporary"
+          },
+          "genes": {
+            "href": "https://api.artsy.net/api/genes?artist_id=5857f54a9c18db601c001594"
+          }
+        }
+      },
+      {
+        "id": "5856aa43c9dc244dbf000bb1",
+        "slug": "peter-layzell",
+        "created_at": "2016-12-18T15:24:51+00:00",
+        "updated_at": "2017-10-31T14:34:14+00:00",
+        "name": "Peter Layzell",
+        "sortable_name": "Layzell Peter",
+        "gender": "male",
+        "biography": "",
+        "birthday": "1962",
+        "deathday": "",
+        "hometown": "UK",
+        "location": "UK",
+        "nationality": "British",
+        "image_versions": [
+          "square",
+          "tall",
+          "large",
+          "four_thirds"
+        ],
+        "_links": {
+          "thumbnail": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/QjHisDlJRzXaqeAAd_22Mw/square.jpg"
+          },
+          "image": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/QjHisDlJRzXaqeAAd_22Mw/{image_version}.jpg",
+            "templated": true
+          },
+          "self": {
+            "href": "https://api.artsy.net/api/artists/5856aa43c9dc244dbf000bb1"
+          },
+          "permalink": {
+            "href": "http://www.artsy.net/artist/peter-layzell"
+          },
+          "artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=5856aa43c9dc244dbf000bb1"
+          },
+          "published_artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=5856aa43c9dc244dbf000bb1&published=true"
+          },
+          "similar_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=5856aa43c9dc244dbf000bb1"
+          },
+          "similar_contemporary_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=5856aa43c9dc244dbf000bb1&similarity_type=contemporary"
+          },
+          "genes": {
+            "href": "https://api.artsy.net/api/genes?artist_id=5856aa43c9dc244dbf000bb1"
+          }
+        }
+      },
+      {
+        "id": "5855ae318b3b8165270009b2",
+        "slug": "rabindranath-tagore",
+        "created_at": "2016-12-17T21:29:21+00:00",
+        "updated_at": "2017-10-31T14:28:22+00:00",
+        "name": "Rabindranath Tagore",
+        "sortable_name": "Tagore Rabindranath",
+        "gender": "male",
+        "biography": "",
+        "birthday": "1861",
+        "deathday": "1941",
+        "hometown": "Kolkata, India",
+        "location": "Kolkata, India",
+        "nationality": "Indian",
+        "image_versions": [
+          "tall",
+          "large",
+          "square",
+          "four_thirds"
+        ],
+        "_links": {
+          "thumbnail": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/Rus13BmUIeoEV9Z0z3em8w/tall.jpg"
+          },
+          "image": {
+            "href": "https://d32dm0rphc51dk.cloudfront.net/Rus13BmUIeoEV9Z0z3em8w/{image_version}.jpg",
+            "templated": true
+          },
+          "self": {
+            "href": "https://api.artsy.net/api/artists/5855ae318b3b8165270009b2"
+          },
+          "permalink": {
+            "href": "http://www.artsy.net/artist/rabindranath-tagore"
+          },
+          "artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=5855ae318b3b8165270009b2"
+          },
+          "published_artworks": {
+            "href": "https://api.artsy.net/api/artworks?artist_id=5855ae318b3b8165270009b2&published=true"
+          },
+          "similar_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=5855ae318b3b8165270009b2"
+          },
+          "similar_contemporary_artists": {
+            "href": "https://api.artsy.net/api/artists?similar_to_artist_id=5855ae318b3b8165270009b2&similarity_type=contemporary"
+          },
+          "genes": {
+            "href": "https://api.artsy.net/api/genes?artist_id=5855ae318b3b8165270009b2"
+          }
+        }
+      }
+    ];
